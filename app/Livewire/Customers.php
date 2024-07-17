6507bdb98ddf930565662346b1b38c5329875300
MamunurHamid <hshihab064@gmail.com>
2024-07-18 02:50:00 +0600

    search updated

--- app/Livewire/Customers.php
+++ app/Livewire/Customers.php
@@ -5,33 +5,30 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\Customer;
 use Auth;
+
 class Customers extends Component
 {
-    public $customers=[];
-    public $search='';
-     
-    /*
+    public $customers = [];
+    public $search = '';
 
-    public function mount(){
-        $this->customers=Customer::all();
-    }
-*/
     public function render()
     {
-        //dd(Auth::user()->name);
-
-        if(! $this->search){
-            $this->customers=Customer::all();
-        }else{
-            $this->customers=Customer::where('name','like','%'.$this->search.'%')->get();
+        if (!$this->search) {
+            $this->customers = Customer::all();
+        } else {
+            $this->customers = Customer::where('name', 'like', '%' . $this->search . '%')
+                ->orWhere('email', 'like', '%' . $this->search . '%')
+                ->orWhere('phone', 'like', '%' . $this->search . '%')
+                ->get();
         }
-        
+
         return view('livewire.customers');
     }
 
-    public function deletecustomer(Customer $customer){
+    public function deletecustomer(Customer $customer)
+    {
         $customer->delete();
-        session()->flash('success','Customer Deleted Successfully');
-        return $this->redirect('/customers',navigate:true);
+        session()->flash('success', 'Customer Deleted Successfully');
+        return $this->redirect('/customers', navigate: true);
     }
 }
